5e97fc1aecda1c5262e19b79ec2a66fe4980c4aa
Diana Arreola <arreoladiana11@gmail.com>
2022-06-06 16:09:46 -0700

    asic html structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,13 @@
+<div>
+  <form action="" method="">
+    <input type="hidden" name="done" value=""/>
+  </form>
+  <button aria-label="Mark done/not done">done/notdone</button>
+  <form action="">
+    <input type="text"/>
+    <button aria-label="Save Todo item">Save</button>
+  </form>
+  <form action="">
+    <button aria-label="Delete Todo item">Delete</button>
+  </form>
+</div>
